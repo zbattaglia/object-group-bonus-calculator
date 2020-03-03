36a2375c1e371a429efc80d3aa402ccb695a5122
Ashley Scott <aescott87@gmail.com>
2020-03-03 16:58:21 -0600

    added several functions, a few index elements, and sourced in jQuery

--- client.js
+++ client.js
@@ -40,4 +40,87 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+$(document).ready(readyNow)
+
 console.log( employees );
+
+let employeeBonusArray = [];
+for(let i = 0; i < employees.length; i++) {
+  console.log(employeeBonus(employees[i]));
+  employeeBonusArray.push(employeeBonus(employees[i]));
+}
+
+console.log('employeeBonusArray:', employeeBonusArray);
+
+//creates new employee objects
+function Employee(name, bonusPercentage, totalCompensation, totalBonus) {
+  this.name = name;
+  this.bonusPercentage = bonusPercentage;
+  this.totalCompensation = totalCompensation;
+  this.totalBonus = totalBonus;
+} //end Employee
+
+//pushes new employee info into new object
+function employeeBonus(obj) {
+  let percentage = bonusCalculation(obj.employeeNumber, obj.reviewRating, Number(obj.annualSalary));
+  let bonus = Math.round( Number(obj.annualSalary) * (percentage/100) );
+  let totalSalary = Number(obj.annualSalary) + bonus;
+  return new Employee( obj.name, percentage, totalSalary, bonus);
+} //end employeeBonus
+
+//calculate bonus percentage
+function bonusCalculation(employeeNumber, rating, annualIncome) {
+  let bonusPercent = 0;
+  if(rating <= 2) {
+    bonusPercent = 0;
+  } else if( rating === 3) {
+    bonusPercent = 4;
+  } else if(rating === 4) {
+    bonusPercent = 6;
+  } else if(rating === 5) {
+    bonusPercent = 10;
+  }
+  if(employeeNumber.length === 4) {
+    bonusPercent += 5;
+  }
+  if(annualIncome > 65000) {
+    bonusPercent -= 1;
+  }
+  if(bonusPercent > 13) {
+    bonusPercent = 13;
+  } else if(bonusPercent < 0) {
+    bonusPercent = 0;
+  }
+  return bonusPercent;
+}//end bonusCalculation
+
+console.log(bonusCalculation(89068, 1, 35000));
+console.log(bonusCalculation('6243', 5, 75000));
+console.log(employeeBonus(  {
+  name: 'Mayella',
+  employeeNumber: '89068',
+  annualSalary: '35000',
+  reviewRating: 1
+}));
+
+console.log(employeeBonus({
+  name: 'Atticus',
+  employeeNumber: '2405',
+  annualSalary: '47000',
+  reviewRating: 3
+}));
+
+
+function readyNow() {
+
+  let el = $( "#employeeOut");
+  el.empty(); 
+  console.log(el);
+  for (let i = 0; i < employeeBonusArray.length; i++) { 
+    el.append(`<li>` + employeeBonusArray[i].name + ' ' + employeeBonusArray[i].percentage + `</li>`)
+    console.log(el);
+
+  }
+}
+
+
